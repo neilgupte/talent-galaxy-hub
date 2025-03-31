05ae0e0bee4eff08e6eebc909fdbd8bf313c4da8
neilgupte <neil.gupte@gmail.com>
2025-03-31 12:35:43 +0100

    Update useJobsQuery.ts

--- src/hooks/useJobsQuery.ts
+++ src/hooks/useJobsQuery.ts
@@ -98,7 +98,7 @@ export const useJobsQuery = ({
         query = query.range(from, to);
         
         const { data, error, count } = await query;
-        console.log('JOB DATA:', data);
+        console.log('🔥 Supabase returned:', data, 'Count:', count);
         
         if (error) {
           console.error('Error fetching jobs:', error);
